419ead2413f40b3c5b44d2f42ff72d6f39d932cd
Hubert Sokołowski <hubert.sokool@gmail.com>
2023-06-11 18:04:54 +0200

    dodanie seriloga

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Serilog;
+    
+    Log.Logger = new LoggerConfiguration()
+.MinimumLevel.Debug()
+.WriteTo.File("log-test.txt")
+.CreateLogger();
+
+try
+{
+Log.Information("Starting up"); 
+}
+catch (Exception ex)
+{
+Log.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+Log.CloseAndFlush();
+}
+Console.WriteLine("Hello World!");
+Console.ReadKey();
 Console.WriteLine("Hello, World!");
